d3ddf3883d9936c2c519ab416e7116f37cbbcf6c
ShibilaTM <shibilaang@gmail.com>
2023-12-29 13:07:59 +0530

    first commit

--- backend/routes/todoroutes.js
+++ backend/routes/todoroutes.js
@@ -7,6 +7,7 @@ router.use(cors())
 router.post('/add',async(req,res)=>{
     try {
         const data = req.body
+        
         const saveForm = await todoData(data).save()
         res.status(200).json({message:'successfully added'})
         
@@ -23,45 +24,14 @@ router.get('/getall',async(req,res)=>{
         if(!saveForm){
             return res.status(404).json({message:'user data not found'})
         }
-        res.status(200).json(saveForm)
+    
+        res.status(200).json(saveForm )
     } catch (error) {
         res.status(500).json({error:error})
 
     }  
 })
 
-//get one 
-router.get('/getone/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const user = await todoData.findById(id);
-
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-
-//update method
-router.put('/update/:id',async(req,res)=>{
-    try {
-        const id = req.params.id
-        const userExist = await todoData.findById(id)
-        if(!userExist){
-            res.status(404).json({message:'user not found'})
-        }
-        const data=req.body
-        const updateForm = await todoData.findByIdAndUpdate(id,data,{new:true})
-        res.status(200).json({message:'successfully updated'})
-    } catch (error) {
-        res.status(500).json({error:error})
-    }
-})
 
 //delete method
 router.delete('/delete/:id',async(req,res)=>{
--- frontend/src/elements/TodoItems.jsx
+++ frontend/src/elements/TodoItems.jsx
@@ -1,63 +1,184 @@
-import React, { useEffect, useState } from 'react'
-import '../CSS/TodoItems.css'
-import DeleteIcon from '@mui/icons-material/Delete';
-import toast from 'react-hot-toast';
-import axios from 'axios'
-import { Link } from 'react-router-dom';
-const TodoItems = () => {
-  const [todos,setTodos] = useState([])
+// import React, { useEffect, useState } from 'react'
+// import '../CSS/TodoItems.css'
+// import DeleteIcon from '@mui/icons-material/Delete';
+// import toast from 'react-hot-toast';
+// import axios from 'axios'
+// import { Link } from 'react-router-dom';
+// const TodoItems = () => {
+//   const [todos,setTodos] = useState([])
 
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await axios.get('http://localhost:4000/todo/getall');
-          setTodos(response.data);
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      };
+//     useEffect(() => {
+//       const fetchData = async () => {
+//         try {
+//           const response = await axios.get('http://localhost:4000/todo/getall');
+//           setTodos(response.data);
+//         } catch (error) {
+//           console.error('Error fetching data:', error);
+//         }
+//       };
     
-      fetchData();
-    }, []);
+//       fetchData();
+//     }, []);
   
-  const deleteUser=async(userId)=>{
-    await axios.delete(`http://localhost:4000/todo/delete/${userId}`)
-    .then((response)=>{
-      setTodos((prevUser)=>prevUser.filter((todo)=>todo._id!==userId))
-      toast.success(response.data.message,{position:'top-right'})
-    }).catch((error)=>{
-      console.log(error)
-    })
-  }
-  return (  
-    <div className='userTable'>
-      <Link to={'/'} className='addButton'>Add Task</Link>
-    <table border={1} cellPadding={15} cellSpacing={0}>
-      <thead>
-          <tr>
-              <th>SI No:</th>
-              <th>Todo List</th>
-              <th>Actions</th>
-          </tr>
-      </thead>
-      <tbody>
-      {Array.isArray(todos) && todos.map((todo, index) => {
-              return(
-                <tr key={todo._id}>
-                <td>{index+1}</td>
-                <td>{todo.addyourtask}</td>
-                <td className='actionButtons'>
-                <button onClick={()=>{deleteUser(todo._id)}}><DeleteIcon/></button>
-                </td>
-            </tr>
-              )
-            })
-            }
+//   const deleteUser=async(userId)=>{
+//     await axios.delete(`http://localhost:4000/todo/delete/${userId}`)
+//     .then((response)=>{
+//       setTodos((prevUser)=>prevUser.filter((todo)=>todo._id!==userId))
+//       toast.success(response.data.message,{position:'top-right'})
+//     }).catch((error)=>{
+//       console.log(error)
+//     })
+//   }
+//   return (  
+//     <div className='userTable'>
+//       <Link to={'/'} className='addButton'>Add Task</Link>
+//     <table border={1} cellPadding={15} cellSpacing={0}>
+//       <thead>
+//           <tr>
+//               <th>SI No:</th>
+//               <th>Todo List</th>
+//               <th>Actions</th>
+//           </tr>
+//       </thead>
+//       <tbody>
+//       {Array.isArray(todos) && todos.map((todo, index) => {
+//               return(
+//                 <tr key={todo._id}>
+//                 <td>{index+1}</td>
+//                 <td>{todo.addyourtask}</td>
+//                 <td className='actionButtons'>
+//                 <button onClick={()=>{deleteUser(todo._id)}}><DeleteIcon/></button>
+//                 </td>
+//             </tr>
+//               )
+//             })
+//             }
     
-      </tbody>
-    </table>
-  </div>
-  )
-}
+//       </tbody>
+//     </table>
+//   </div>
+//   )
+// }
 
-export default TodoItems
+// export default TodoItems
+
+import React, { useEffect, useState } from 'react';
+import '../CSS/TodoItems.css';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Checkbox from '@mui/material/Checkbox';
+import toast from 'react-hot-toast';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+const TodoItems = () => {
+  const [todos, setTodos] = useState([]);
+  const [filter, setFilter] = useState('all'); // 'all', 'completed', 'incomplete'
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:4000/todo/getall');
+        setTodos(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const updateUser = async (userId, completed) => {
+    await axios
+      .put(`http://localhost:4000/todo/update/${userId}`, { completed })
+      .then((response) => {
+        setTodos((prevTodos) =>
+          prevTodos.map((todo) =>
+            todo._id === userId ? { ...todo, completed } : todo
+          )
+        );
+        toast.success(response.data.message, { position: 'top-right' });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const deleteUser = async (userId) => {
+    await axios
+      .delete(`http://localhost:4000/todo/delete/${userId}`)
+      .then((response) => {
+        setTodos((prevUser) =>
+          prevUser.filter((todo) => todo._id !== userId)
+        );
+        toast.success(response.data.message, { position: 'top-right' });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const filteredTodos = todos.filter((todo) => {
+    if (filter === 'all') {
+      return true;
+    } else if (filter === 'completed') {
+      return todo.completed;
+    } else if (filter === 'incomplete') {
+      return !todo.completed;
+    }
+    return true;
+  });
+
+  return (
+    <div className='userTable'>
+      <Link to={'/'} className='addButton'>
+        Add Task
+      </Link>
+      <div className='filterButtons'>
+        <button onClick={() => setFilter('all')}>All</button>
+        <button onClick={() => setFilter('completed')}>Completed</button>
+        <button onClick={() => setFilter('incomplete')}>Incomplete</button>
+      </div>
+      <table border={1} cellPadding={15} cellSpacing={0}>
+        <thead>
+          <tr>
+            <th>SI No:</th>
+            <th>Todo List</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.isArray(filteredTodos) &&
+            filteredTodos.map((todo, index) => {
+              return (
+                <tr key={todo._id}>
+                  <td>{index + 1}</td>
+                  <td>
+                    {todo.completed ? (
+                      <del>{todo.addyourtask}</del>
+                    ) : (
+                      todo.addyourtask
+                    )}
+                  </td>
+                  <td className='actionButtons'>
+                    <Checkbox
+                      checked={todo.completed}
+                      onChange={(e) =>
+                        updateUser(todo._id, e.target.checked)
+                      }
+                    />
+                    <button onClick={() => deleteUser(todo._id)}>
+                      <DeleteIcon />
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TodoItems;
+
+
